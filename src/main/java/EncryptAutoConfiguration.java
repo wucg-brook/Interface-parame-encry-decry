7088a49b84d80635750636477e6cf86dccdaceb0
wuchenggong@tzxw.onaliyun.com <wuchenggong@tzxw.onaliyun.com>
2022-06-22 11:07:47 +0800

    commit

---
+++ src/main/java/EncryptAutoConfiguration.java
@@ -0,0 +1,8 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.javaboy.encrypt.starter")
+public class EncryptAutoConfiguration {
+
+}
